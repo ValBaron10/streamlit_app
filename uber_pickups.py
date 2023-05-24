3faf73710a1bd653a2cc9ee01d2c4c358576129c
ValBaron10 <valentin.baron10@gmail.com>
2023-05-24 14:26:02 +0200

    Link with API

--- uber_pickups.py
+++ uber_pickups.py
@@ -1,38 +1,41 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import requests
 
-st.title('Uber pickups in NYC')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+def fetch(session, url, inputs):
+    try:
+        result = session.get(url)
+        st.write(inputs)
+        return requests.post(url, json=inputs)
+    except Exception:
+        return {}
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+st.title('Flower prediction')
 
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! (using st.cache_data)")
+session = requests.Session()
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+with st.form("my_form"):
+  # Buttons to enter figures
+  sepal_length = st.number_input('Insert sepal length')
+  st.write('Sepal length = ', sepal_length)
+  sepal_width = st.number_input('Insert sepal width')
+  st.write('Sepal width = ', sepal_width)
+  petal_length = st.number_input('Insert petal length')
+  st.write('Petal length = ', petal_length)
+  petal_width = st.number_input('Insert petal width')
+  st.write('Petal width = ', petal_width)
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+  new_measurement = {
+      'sepal_length': sepal_length,
+      'sepal_width': sepal_width,
+      'petal_length': petal_length,
+      'petal_width': petal_width
+  }
 
-hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-st.map(filtered_data)
+  submitted = st.form_submit_button("Submit")
+
+  if submitted:
+    response = fetch(session, 'https://firstappvba.azurewebsites.net/predict', new_measurement)
+    st.write(response.content)
